ea75bc2247b3259cfb21e20f9eeeeebf312016d4
zyxbmstu <zaxarler@mail.ru>
2021-12-16 20:20:45 +0300

    Scripted auto-commit on change (2021-12-16 20:20:45) by gitwatch.sh

--- src/main/java/bmstu/iu9/CacheActor.java
+++ src/main/java/bmstu/iu9/CacheActor.java
@@ -2,13 +2,18 @@ package bmstu.iu9;
 
 import akka.actor.AbstractActor;
 import akka.japi.pf.ReceiveBuilder;
+import bmstu.iu9.requests.Request;
 
 public class CacheActor extends AbstractActor {
+
+    private static final Long = default
 
     @Override
     public Receive createReseive() {
         return ReceiveBuilder.create()
-                .match()
+                .match(Request.class, (request) -> {
+                    Long
+                })
     }
 
 }
