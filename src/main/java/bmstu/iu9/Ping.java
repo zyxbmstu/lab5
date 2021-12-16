e1d7fe2a2a6e9aa1de9600dc56a57273fa668310
zyxbmstu <zaxarler@mail.ru>
2021-12-16 20:14:27 +0300

    Scripted auto-commit on change (2021-12-16 20:14:27) by gitwatch.sh

--- src/main/java/bmstu/iu9/Ping.java
+++ src/main/java/bmstu/iu9/Ping.java
@@ -1,4 +1,15 @@
 package bmstu.iu9;
 
+import akka.actor.ActorRef;
+import akka.actor.ActorSystem;
+import akka.actor.Props;
+
 public class Ping {
+
+    private ActorRef cacheActor;
+
+    public Ping(ActorSystem system) {
+        cacheActor = system.actorOf(Props.create(CacheActor.class));
+    }
+
 }
