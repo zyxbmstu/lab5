a3f6b9e46f07e49c8b9967267434273407052c2e
zyxbmstu <zaxarler@mail.ru>
2021-12-16 20:22:13 +0300

    Scripted auto-commit on change (2021-12-16 20:22:13) by gitwatch.sh

--- src/main/java/bmstu/iu9/CacheActor.java
+++ src/main/java/bmstu/iu9/CacheActor.java
@@ -4,15 +4,19 @@ import akka.actor.AbstractActor;
 import akka.japi.pf.ReceiveBuilder;
 import bmstu.iu9.requests.Request;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class CacheActor extends AbstractActor {
 
-    private static final Long = default
+    private static final Long defaultValue = -1L;
+    private Map<String, Long> cache = new HashMap<>();
 
     @Override
     public Receive createReseive() {
         return ReceiveBuilder.create()
                 .match(Request.class, (request) -> {
-                    Long
+                    Long requestResult = cache.getOrDefault()
                 })
     }
 
