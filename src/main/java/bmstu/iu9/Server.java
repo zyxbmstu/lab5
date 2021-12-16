df160ab2bb5d802ba7299a85adcc2cd4e8e07546
zyxbmstu <zaxarler@mail.ru>
2021-12-16 19:44:46 +0300

    Scripted auto-commit on change (2021-12-16 19:44:45) by gitwatch.sh

--- src/main/java/bmstu/iu9/Server.java
+++ src/main/java/bmstu/iu9/Server.java
@@ -5,7 +5,8 @@ import java.io.IOException;
 public class Server {
 
     public static void main(String[] args) throws IOException {
-        
+        System.out.println("Start!");
+        ActorSystem
     }
 
 }
