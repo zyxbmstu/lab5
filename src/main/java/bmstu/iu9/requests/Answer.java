800b972f3cb75cd1f68ba08557eb2adcb21711ee
zyxbmstu <zaxarler@mail.ru>
2021-12-16 20:26:08 +0300

    Scripted auto-commit on change (2021-12-16 20:26:08) by gitwatch.sh

--- src/main/java/bmstu/iu9/requests/Answer.java
+++ src/main/java/bmstu/iu9/requests/Answer.java
@@ -1,4 +1,21 @@
 package bmstu.iu9.requests;
 
 public class Answer {
+
+    private String url;
+    private Long responseTime;
+
+    public Answer(String url, Long responseTime) {
+        this.url = url;
+        this.responseTime = responseTime;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public Long getResponseTime() {
+        return responseTime
+    }
+
 }
