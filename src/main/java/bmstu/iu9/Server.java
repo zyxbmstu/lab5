326a841913546c7a0f46582794a3d9f4adfdd26c
zyxbmstu <zaxarler@mail.ru>
2021-12-16 19:46:57 +0300

    Scripted auto-commit on change (2021-12-16 19:46:57) by gitwatch.sh

--- src/main/java/bmstu/iu9/Server.java
+++ src/main/java/bmstu/iu9/Server.java
@@ -1,12 +1,16 @@
 package bmstu.iu9;
 
+import akka.actor.ActorSystem;
+
 import java.io.IOException;
 
 public class Server {
 
+    private static final String ACTOR_SYSTEM_NAME = ""
+
     public static void main(String[] args) throws IOException {
         System.out.println("Start!");
-        ActorSystem
+        ActorSystem system = ActorSystem.create();
     }
 
 }
